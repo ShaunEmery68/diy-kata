d3ef12b00849649d28a058564840aa006afa5d83
Shaun Emery <shaunsheep18@gmail.com>
2021-12-20 18:24:52 +0000

    refactored kata 6

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,15 +1,20 @@
-const joinNames = namesObj => {;
- let newString = '';
- const  arrayLength = namesObj.length;
- for (let i = 0; i < arrayLength; i++) {
-     newString += namesObj[i].name;
-     if (i < arrayLength -2) {
-         newString += ', ';
-     }
-     if (i === arrayLength -2) {
-        newString += ' & ';
-     }
- }
- return newString;
- }
+
+const joinNames = namesObj => {
+// let newString = '';
+// const  arrayLength = namesObj.length;
+//for (let i = 0; i < arrayLength; i++) {
+ //    newString += namesObj[i].name;
+ //    if (i < arrayLength -2) {
+ //        newString += ', ';
+ //    }
+  //   if (i === arrayLength -2) {
+ //       newString += ' & ';
+  //   }
+ //}
+ //eturn newString;
+ //}
+
+
+}
+
 module.exports = joinNames;
